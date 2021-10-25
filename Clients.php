81c6243a948038540bf3b8df818dd9ac8bf606a4
alberto <albertozanola1996@gmail.com>
2021-10-25 08:31:17 +0200

    Inserimento Clients

--- Clients.php
+++ Clients.php
@@ -3,12 +3,6 @@
     
     class Clients extends Products {
         public $codClients;
-        public $discount = 0;
-
-        function __construct($_codClients)
-        {
-            $this->codClients = $_codClients; 
-        }
 
         function setClients($_codClients)
         {
@@ -20,27 +14,15 @@
             return $this->codClients;
         }
 
-        function setDiscount($_discount) {
-            $this->discount = $_discount;
-            if ($_codClients = ['x1xx','z2zz', 'a3aa']) {
-                $this->discount = 30;
-                echo 'Sconto del 30% applicato.';
-            } else {
-                echo 'Prezzo base.';
-            } 
-        }
-
-        function getDiscount() {
-            return $this->discount;
-        }
     }
 
-    $myPrice2 = new Clients ('es89ggcz', 0, 'carta', 'as89bc');
-    var_dump($myPrice2);
-
-    $myPrice = new Clients('asf89cc3t7', 'plastica' );
-    echo $myPrice->setDiscount('x1xx');
+    $myPrice = new Clients ('as55b88gg', 'carta','a44x');
+    echo $myPrice->setClients('xx4rz');
     var_dump($myPrice);
+
+    $myPrice2 = new Clients('asf89cc3t7', 'plastica', 'x4er');
+    echo $myPrice2->setClients('x1xx');
+    var_dump($myPrice2);
 
     
     
