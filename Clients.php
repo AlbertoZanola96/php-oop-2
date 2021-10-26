2f763538a75dc4bc5193710ce66fea6be07e29aa
alberto <albertozanola1996@gmail.com>
2021-10-26 10:07:25 +0200

    Modifica construct

--- Clients.php
+++ Clients.php
@@ -4,6 +4,12 @@
     class Clients extends Products {
         public $codClients;
 
+        public function __construct($_codProduct, $_category, $_codClients) 
+        {
+            parent::__construct($_codProduct, $_category);
+            $this->codClients = $_codClients;
+        }
+
         function setClients($_codClients)
         {
             $this->codClients = $_codClients;
@@ -13,18 +19,14 @@
         {
             return $this->codClients;
         }
-
     }
 
-    $myPrice = new Clients ('as55b88gg', 'carta','a44x');
-    echo $myPrice->setClients('xx4rz');
+    $myPrice = new Clients ('as55b88gg', 'carta', 'a44x');
+    // echo $myPrice->setClients('xx4rz');
     var_dump($myPrice);
 
-    $myPrice2 = new Clients('asf89cc3t7', 'plastica', 'x4er');
-    echo $myPrice2->setClients('x1xx');
+    $myPrice2 = new Clients('asf89cc3t7', 'plastica','x4er');
+    // echo $myPrice2->setClients('x1xx');
     var_dump($myPrice2);
-
-    
-    
 
 ?>
--- Warehouse.php
+++ Warehouse.php
@@ -4,6 +4,12 @@
     class Warehouse extends Products {
         public $quantity;
 
+        public function __construct($_codProduct, $_category, $_quantity) 
+        {
+            parent::__construct($_codProduct, $_category);
+            $this->quantity = $_quantity;
+        }
+
         function setQuantity($_quantity)
         {
             $this->quantity = $_quantity;
@@ -16,7 +22,7 @@
     }
 
     $myWarehouse = new Warehouse('aaabbbccc', 'ferro', 10);
-    echo $myWarehouse->setQuantity(9);
+    // echo $myWarehouse->setQuantity(9);
     var_dump($myWarehouse);
 
 ?>
